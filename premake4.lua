f62442ecfb28e78c1ffdeea0577a979e5229d25b
Paulino Marié <paulinodjm@hotmail.com>
2013-06-22 18:14:12 +0200

    the project use premake4 now

---
+++ premake4.lua
@@ -0,0 +1,15 @@
+-- this premake file is quite bad, because I just start with premake.
+
+solution "Color-Project"
+
+   configurations {"Release"}
+ 
+   project "game"
+      kind "WindowedApp"
+      language "C++"
+      files {"src/**.h", "src/**.cpp"}
+ 
+      configuration "Release"
+         flags { "Optimize" }
+         buildoptions "-std=c++11"  
+         links { "sfml-graphics", "sfml-window", "sfml-system", "jsoncpp" }
