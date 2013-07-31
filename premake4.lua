209aa6b8a1599ffb4d562787736fdd6aff014c4a
Paulino Marié <paulinodjm@hotmail.com>
2013-07-31 11:53:41 +0200

    solves issues when compiling on windows

--- premake4.lua
+++ premake4.lua
@@ -1,4 +1,6 @@
 -- this premake file is quite bad, because I just start with premake and lua.
+-- This file is written to build on archlinux and windows8 (with codeblocks).
+-- I use static linking on windows.
 
 solution "Color-Project"
   configurations {"Release"}
@@ -11,7 +13,10 @@ solution "Color-Project"
 
     configuration "Release"
       flags { "Optimize" }
-      buildoptions "-std=c++11"  
+      buildoptions "-std=c++11"
+      if os.get() == "windows" then
+        defines { "SFML_STATIC" }
+      end
 
   -- game --
   project "game"
@@ -21,11 +26,11 @@ solution "Color-Project"
 
     configuration "Release"
       flags { "Optimize" }
-      buildoptions "-std=c++11"  
+      buildoptions "-std=c++11"
       links { "engine", "jsoncpp" }
-      if os.get == "windows" then
-        links { "sfml-graphics-s", "sfml-window-s", "sfml-system-s", "sfml-audio-s" }
+      if os.get() == "windows" then
         defines { "SFML_STATIC" }
+        links { "sfml-graphics-s", "sfml-window-s", "sfml-system-s", "sfml-audio-s" }
       else
         links { "sfml-graphics", "sfml-window", "sfml-system", "sfml-audio" }
       end
