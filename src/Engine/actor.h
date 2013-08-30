0858a5ff5c11a002ef684fdf1933a897e5baba4d
Marié Paulino <paulinodjm@hotmail.com>
2013-08-30 16:47:04 +0200

    add the actor interface

---
+++ src/Engine/actor.h
@@ -0,0 +1,12 @@
+//////////////////////////////////////////////////////////
+/// Copyright 2013 Marié Paulino (paulinodjm@hotmail.com)
+/// Licence : Simplified BSD Licence (see inclued LICENCE)
+//////////////////////////////////////////////////////////
+#pragma once
+
+class ACTOR
+{
+public:
+
+  virtual void Update() = 0;
+};
